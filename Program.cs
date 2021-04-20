1f005d797f495f44a1fcfebf8609a71fe5db1277
unknown <ademodiseun@gmail.com>
2021-04-20 13:21:36 -0700

    another push

--- Program.cs
+++ Program.cs
@@ -6,7 +6,76 @@ namespace ControlStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // int number = 10;
+            // if (number ==10)
+            // {
+            //    Console.WriteLine("Number is 10");  
+            // }
+            // else if (number==20)
+            // {
+            //      Console.WriteLine("Number is 20");
+            // }
+            // else if (number==30)
+            // {
+            //      Console.WriteLine("Number is 30");
+            // }
+            // else
+            // {
+            //      Console.WriteLine("pls enter a Number ");
+            // }
+            
+                
+            
+            
+                
+            // int number = 20;
+            // switch (number)
+            // {
+            //     case 10:
+            //      Console.WriteLine("Number is 10");
+            //         break;
+            //         case 20:
+            //          Console.WriteLine("Number is 20");
+            //          break;
+            //          case 30:
+            //           Console.WriteLine("Number is 30");
+            //           break;
+            //           default:
+            //            Console.WriteLine(" invalid Number ");
+            //           break;
+            // }
+            
+           
+                // int a = 10;
+                // while (a<20)
+                // {
+                //      Console.WriteLine("value of a is " + a);
+                //      a++;
+                // }
+            //  int a = 10;
+            //  do
+            //  {
+            //      Console.WriteLine("value of a is " + a);
+            //      a++; 
+            //  }
+            //   while (a<20);
+            
+
+            
+
+            //for loop
+            //  for (int i=0; i <5 ; i++)
+            //  {
+            //     Console.WriteLine(i);  
+            //  }
+
+             // foreach loop
+                 string[] cars = {"volvo","benz","kia"};
+                  foreach (string i in cars)
+                    {
+                     Console.WriteLine(i); 
+                    }
+
         }
     }
 }
